59d31b5bf5d5d930dc9fa21027c1e7a8b241d090
Bu4i <st128493@student.spbu.ru>
2025-05-12 06:33:03 +0300

    Delete

--- large_gen.h
+++
@@ -1,9 +0,0 @@
-#ifndef LARGE_GEN_H
-#define LARGE_GEN_H
-
-#include <Eigen/Dense>
-#include <utility>
-
-std::pair<Eigen::MatrixXd, Eigen::VectorXd> generateRandomSystem(int size, unsigned int seed = 42);
-
-#endif 
